d68de04d1f69bce2beed8d45f5f5bc6c4ce84bff
lizibo <lizibo@hsbank360.com>
2017-10-11 14:57:28 +0800

    .

--- risk-rsfservice/src/main/java/com/risk/provider/model/UserDto.java
+++ risk-rsfservice/src/main/java/com/risk/provider/model/UserDto.java
@@ -10,6 +10,7 @@ import java.util.Date;
  */
 @Data
 public class UserDto implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String name                ;
     private String cardName           ;
     private String cardNumber         ;
